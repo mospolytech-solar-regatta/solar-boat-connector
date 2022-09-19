57afcd2cdfe53440228939e9373a66fcc1662971
Dmitry Paramonov <gamiysikdima@gmail.com>
2022-09-19 21:41:47 +0400

    requirements.txt fix

--- app_config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-redis_dsn = os.environ.get('REDIS_DSN', '')
-postgres_dsn = f'postgresql://{os.environ.get("POSTGRES_USER")}:{os.environ.get("POSTGRES_PASSWORD")}@' \
-               f'{os.environ.get("POSTGRES_SERVER")}:{os.environ.get("POSTGRES_PORT")}/{os.environ.get("POSTGRES_DB")}'
-serial_port = os.environ.get('SERIAL_PORT')
-serial_rate = os.environ.get('SERIAL_RATE')
